51fd33f566a3365a66e17bb1b456e2b6de29bce1
Leo Tafti <leotafti@gmail.com>
2019-02-20 16:19:39 +0100

    Fixed it

--- src/main/scala/example/Lists.scala
+++ src/main/scala/example/Lists.scala
@@ -22,7 +22,10 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = xs match{
+    case Nil => 0
+    case x::xs => x + sum(xs)
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -37,5 +40,13 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+    def maxRec(temp : Int, l : List[Int]) : Int = l match {
+      case Nil => temp
+      case x::xs => if (x > temp) maxRec(x, xs) else maxRec(temp, xs)
+    }
+
+    if(xs.isEmpty) throw new NoSuchElementException
+    else maxRec(Integer.MIN_VALUE, xs)
+  }
 }
