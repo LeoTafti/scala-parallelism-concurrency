45af92677657e5e3e2307c71f60ee4e9b179409e
Guillaume Martres <smarter@ubuntu.com>
2019-05-06 21:52:19 +0200

    Fix file decoding under Windows

--- src/main/scala/wikipedia/WikipediaData.scala
+++ src/main/scala/wikipedia/WikipediaData.scala
@@ -1,13 +1,14 @@
 package wikipedia
 
 import scala.io.Source
+import scala.io.Codec.UTF8
 
 object WikipediaData {
 
   private[wikipedia] def lines: List[String] = {
     val resource = getClass.getResourceAsStream("/wikipedia/wikipedia.dat")
     if (resource == null) sys.error("Please download the dataset as explained in the assignment instructions")
-    Source.fromInputStream(resource).getLines().toList
+    Source.fromInputStream(resource)(UTF8).getLines().toList
   }
 
   private[wikipedia] def parse(line: String): WikipediaArticle = {
